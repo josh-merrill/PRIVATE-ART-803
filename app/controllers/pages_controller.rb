3604dab45a5551c17b07ab8a49713fa7c2c32ac7
Tenzin Rose <tenzinrose@gmail.com>
2022-03-01 15:36:12 +0000

    adding-pages-routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def profile
+    @user = current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :artworks_as_artist, class_name: "Artwork", foreign_key: :artist_id
+  has_many :artworks_as_buyer, class_name: "Artwork", foreign_key: :buyer_id
 end
