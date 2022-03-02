f7ab7a4400e379b9f2453b3788df389d32b8a3af
Tempestt Woodard <tnwoodard@gmail.com>
2022-03-02 12:11:37 +0000

    Routes for Auction model and my_auction page are complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'profile', to: 'pages#profile', as: 'profile'
+  get 'my_auctions', to: 'pages#my_auctions', as: 'my_auctions'
   resources :artworks, only: [:index, :new, :create, :show]
+  resources :auctions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
