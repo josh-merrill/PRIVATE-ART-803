9030b9d85394b75ddc34df604235936d899bc5c1
Josh Merrill <joshmmerrill@outlook.com>
2022-03-03 12:09:17 +0000

    FIX: updated auction model to only belong to user and adjusted seed file

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -6,6 +6,5 @@ class Auction < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   has_many :auction_items, dependent: :destroy
-  belongs_to :artist, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :user
 end
