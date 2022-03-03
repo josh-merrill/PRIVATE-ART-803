81feff5a3eeb2b2448433a8e2edb03628f2ea271
Tenzin Rose <tenzinrose@gmail.com>
2022-03-03 13:22:50 +0000

    ADDED: bidding functionality

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,30 +1,27 @@
 class BidsController < ApplicationController
 
   def create
+    @auction = Auction.find(params[:auction_id])
     @auction_item = AuctionItem.find(params[:auction_item_id])
-    @bid = Bid.create(price: bids_params)
+    @bid = Bid.create(bids_params)
     @bid.user = current_user
     @bid.auction_item_id = params[:auction_item_id]
+
     if @bid.save
       BidroomChannel.broadcast_to(
         @auction_item,
         render_to_string(partial: "shared/bid", locals: { bid: @bid })
       )
       head :ok
-      raise
-    end
-    # @bid.save
-    # redirect_to auction_auction_item_path(params["auction_id"], params["auction_item_id"])
+      #redirect_to auction_auction_item_path(@auction, @auction_item)
 
-    # respond_to do |format|
-    #   format.html # Follow regular flow of Rails
-    #   format.text { render partial: 'movies/list', locals: { movies: @movies }, formats: [:html] }
-    # end
+    end
+
   end
 
   private
 
   def bids_params
-    params.require(:price)
+    params.require(:bid).permit(:price)
   end
 end
