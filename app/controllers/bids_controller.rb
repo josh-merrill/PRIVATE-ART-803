8aee4f133b5245234c58c85a96bb24052fed6d4e
Tenzin Rose <tenzinrose@gmail.com>
2022-03-07 13:22:31 +0000

    Added to bidding functionality

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,9 +3,10 @@ class BidsController < ApplicationController
   def create
     @auction = Auction.find(params[:auction_id])
     @auction_item = AuctionItem.find(params[:auction_item_id])
-    @bid = Bid.create(bids_params)
+    @bid = Bid.new(bids_params)
     @bid.user = current_user
     @bid.auction_item_id = params[:auction_item_id]
+
 
     if @bid.save
       BidroomChannel.broadcast_to(
@@ -15,7 +16,8 @@ class BidsController < ApplicationController
       head :ok
       #redirect_to auction_auction_item_path(@auction, @auction_item)
     else
-      render 'auction_items/show', notice: "You can only place one bid"
+      #redirect_to auction_auction_item_path(@auction_item), notice: "You can only place one bid"
+      render 'auction_items/show'
     end
 
   end
