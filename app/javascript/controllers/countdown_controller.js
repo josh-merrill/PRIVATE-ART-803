ad045948cda3946d962f6a4c2afad480f31fd686
Tenzin Rose <tenzinrose@gmail.com>
2022-03-08 17:46:29 +0000

    added

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -16,18 +16,19 @@ connect(){
   console.log(buyer);
 
 
-var countDownDate = new Date(this.endValue).replace(/\s/, 'T').getTime();
-console.log(countDownDate)
+var countDownDate = new Date(this.endValue).getTime();
+console.log(new Date(this.endValue))
 // Update the count down every 1 second
 var x = setInterval(function() {
 // Get todays date and time
-  var now = new Date().replace(/\s/, 'T').getTime();
+  var now = new Date().getTime();
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  console.log(typeof days)
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
