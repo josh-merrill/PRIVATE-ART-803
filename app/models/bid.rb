eb35378d56765836e5969dc82f90963a130e768a
Josh Merrill <joshmmerrill@outlook.com>
2022-03-08 21:15:20 +0000

    FIX: Added validations back to bid model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,5 +3,5 @@ class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :auction_item
   validates :auction_item_id, uniqueness: { scope: :user_id,
-                                            message: "You can only make one bid" }
+  message: "You can only make one bid" }
 end
