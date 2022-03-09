737b8356b5c204605f2c433ae97be78c1bd491fe
Tempestt Woodard <tnwoodard@gmail.com>
2022-03-09 13:28:33 +0000

    BUGS: Fixed bug on bid.price validations

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,5 +1,5 @@
 class Bid < ApplicationRecord
-  validates :price, numericality: { less_than: 9999999 }
+  validates :price, numericality: { less_than: 10000000 }
 
   belongs_to :user
   belongs_to :auction_item
