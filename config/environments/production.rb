8bb9cdcc083d3487bd07376e727b966b001bc458
Tenzin Rose <tenzinrose@gmail.com>
2022-03-10 12:24:50 +0000

    Reverted back routing change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.routes.default_url_options[:host] = 'https://www.artcollective.live/'
+  #config.routes.default_url_options[:host] = 'https://www.artcollective.live/'
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
